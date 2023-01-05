e41e3a70f68777cefd025a67cca5807d4de080b8
Kirti Jain <kirti.jain@ubuy.com>
2023-01-05 16:26:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpackrecyclerview/MyRecyclerView.kt
@@ -0,0 +1,25 @@
+package com.example.jetpackrecyclerview
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.magnifier
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+data class item(val name :String){
+
+}
+
+@Composable
+fun recyclerView(
+    modifier: Modifier,
+    
+){
+
+    LazyColumn(content = )
+
+}
+
+@Composable
+fun model(){
+
+}
